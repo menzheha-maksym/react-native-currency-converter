d91a677257e62442117884790be4bff176b4610b
Maksym Menzheha <masimfyd@gmail.com>
2022-04-14 19:48:48 +0300

    converter now works

--- src/pages/Converter.tsx
+++ src/pages/Converter.tsx
@@ -1,7 +1,7 @@
 import {ParamListBase} from '@react-navigation/native';
 import {NativeStackNavigationProp} from '@react-navigation/native-stack';
-import React from 'react';
-import {Button, StyleSheet, TextInput, View} from 'react-native';
+import React, {useState} from 'react';
+import {Button, StyleSheet, Text, TextInput, View} from 'react-native';
 import MoveTo from '../components/MoveTo';
 
 const styles = StyleSheet.create({
@@ -15,11 +15,20 @@ const styles = StyleSheet.create({
   title: {
     backgroundColor: '#ffffff445',
   },
+  result: {
+    fontWeight: 'bold',
+    fontSize: 32,
+    marginBottom: 30,
+  },
   input: {
     borderWidth: 1,
     backgroundColor: '#ffffff44',
     width: '60%',
     textAlign: 'center',
+  },
+  error: {
+    marginTop: 10,
+    color: 'red',
   },
   button: {
     width: '60%',
@@ -37,15 +46,58 @@ interface ConverterProps {
 
 const Converter: React.FC<ConverterProps> = ({navigation}) => {
   const moveToPage = {name: 'ExchangeRates', title: 'Exchange Rates'};
+
+  const [inputText, onChangeInputText] = useState<string>();
+  const [error, setError] = useState<string>('');
+  const [result, setResult] = useState<string>('');
+
+  async function tryConvert() {
+    if (!inputText) {
+      setError('enter a valid string to convert, (ex "15 usd" in "uah")');
+      return;
+    }
+    let currencies = inputText.split('"');
+    if (currencies.length !== 5) {
+      setError('enter a valid string to convert, (ex "15 usd" in "uah")');
+      return;
+    }
+    currencies = currencies?.slice(1, currencies.length - 1);
+    const value = Number(currencies[0].split(' ')[0]);
+    const fromCurrency = currencies[0].split(' ')[1];
+    const toCurrency = currencies[2];
+    if (isNaN(value)) {
+      setError('enter a valid string to convert, (ex "15 usd" in "uah")');
+      return;
+    }
+    setError('');
+    await fetch(`http://www.floatrates.com/daily/${fromCurrency}.json`)
+      .then(res => res.json())
+      .then(json => {
+        let res = json[toCurrency];
+        let converted = value * Number(res.rate);
+        setResult(
+          `${value} ${fromCurrency.toUpperCase()} = ${converted.toFixed(2)} ${
+            res.code
+          } `,
+        );
+      });
+  }
   return (
     <>
       <View style={styles.container}>
-        <TextInput style={styles.input} placeholder='"15 usd" in "uah"' />
+        <View>
+          <Text style={styles.result}>{result}</Text>
+        </View>
+        <TextInput
+          style={styles.input}
+          placeholder='"15 usd" in "uah"'
+          onChangeText={onChangeInputText}
+        />
+        <View>
+          <Text style={styles.error}>{error}</Text>
+        </View>
         <View style={styles.button}>
-          <Button
-            title="Convert"
-            onPress={() => console.log(' press convert btn')}
-          />
+          <Button title="Convert" onPress={tryConvert} />
         </View>
       </View>
       <View style={styles.navButton}>
