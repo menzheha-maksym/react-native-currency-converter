8148ea01ff78b9801903b01648a5d329a03118e2
Maksym Menzheha <masimfyd@gmail.com>
2022-04-15 15:02:01 +0300

    add some padding to bottom

--- src/pages/ExchangeRates.tsx
+++ src/pages/ExchangeRates.tsx
@@ -14,6 +14,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     flexDirection: 'column',
     paddingTop: 10,
+    paddingBottom: 20,
   },
   navButton: {
     width: '100%',
